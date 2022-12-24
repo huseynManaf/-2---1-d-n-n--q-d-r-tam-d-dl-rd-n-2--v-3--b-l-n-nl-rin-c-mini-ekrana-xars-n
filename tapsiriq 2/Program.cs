22ebf3da402c9bd0371ad8017725fbf71a830bb7
huseynManaf <huseynim@code.edu.az>
2022-12-24 05:23:24 -0800

    init

---
+++ tapsiriq 2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace tapsiriq_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Eded daxil edin:");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+                if (i % 2 == 0 & i % 3 == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine($"cem: {sum}");
+            }
+        }
+    }
+}
